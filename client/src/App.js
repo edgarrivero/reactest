0245802b3e64bc548275cb67c1b9edb145dedd03
Edgar Rivero <riveroedgar.jm@gmail.com>
2021-09-08 22:27:25 -0500

    Client post and get ready

--- client/src/App.js
+++ client/src/App.js
@@ -17,16 +17,29 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import Paper from '@material-ui/core/Paper';
+
 const useStyles = makeStyles((theme) => ({
+
   paper: {
     marginTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
   },
   form: {
     width: '100%', // Fix IE 11 issue.
@@ -35,10 +48,27 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
 }));
-
-
-
 
 function App() {
 
@@ -49,6 +79,8 @@ function App() {
   const [nombre_usuario, setNombre] = useState("")
   const [telefono_usuario, setTelefono] = useState("")
   const [mail_usuario, setMail] = useState("")
+
+  const [users, setUsers] = useState([])
 
 
   const guardar = () => {
@@ -61,52 +93,86 @@ function App() {
     })
     .then(function (response) {
       console.log(response);
+      
     })
     .catch(function (error) {
       console.log(error);
     });
 }
 
+useEffect(() => {
+  Axios.get('http://localhost:3050/tbl_usuario', 
+  {
+      nombre_usuario: nombre_usuario,
+      cedula_usuario: cedula_usuario,
+      teléfono_usuario: telefono_usuario,
+      mail_usuario: mail_usuario
+  })
+  .then(function (response) {
+    setUsers(response.data)
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}, []);
+
+
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  function FormRow() {
+    return (
+      <React.Fragment>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" >
     <CssBaseline />
     <div className={classes.paper}>
-      <Avatar className={classes.avatar}>
-        <LockOutlinedIcon />
-      </Avatar>
+      
       <Typography component="h1" variant="h5">
         Registrar
       </Typography>
       <form className={classes.form} noValidate>
         
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          name="nombre"
-          label="Nombre"
-          type="nombre"
-          id="nombre"
-          autoComplete="current-nombre"
-          onChange={(e) => {
-            setNombre(e.target.value);
-          }}
-        />
-         <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          name="cedula"
-          label="Cedula"
-          type="cedula"
-          id="cedula"
-          autoComplete="current-cedula"
-          onChange={(e) => {
-            setCedula(e.target.value);
-          }}
-        />
+       <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="cedula"
+              label="Cedula"
+              type="cedula"
+              id="cedula"
+              autoComplete="current-cedula"
+              onChange={(e) => {
+                setCedula(e.target.value);
+              }}
+            />
+
+      <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="nombre"
+              label="Nombre"
+              type="nombre"
+              id="nombre"
+              autoComplete="current-nombre"
+              onChange={(e) => {
+                setNombre(e.target.value);
+              }}
+            />
+        
         <TextField
           variant="outlined"
           margin="normal"
@@ -164,6 +230,87 @@ function App() {
         </Grid>
       </form>
     </div>
+
+    <Box
+        display="flex"
+        flexWrap="wrap"
+        alignContent="flex-start"
+        p={1}
+        m={1}
+        bgcolor="background.paper"
+        //sx={{ maxWidth: 300, height: 200 }}
+      >
+        <Box p={1} bgcolor="grey.300">
+        {
+                    users.map((item) => {
+                      return (
+                                  <Card className={classes.root}>
+                                  <CardHeader
+                                    avatar={
+                                      <Avatar aria-label="recipe" className={classes.avatar}>
+                                        {item.nombre_usuario}
+                                      </Avatar>
+                                    }
+                                    action={
+                                      <IconButton aria-label="settings">
+                                        <MoreVertIcon />
+                                      </IconButton>
+                                    }
+                                    title={item.nombre_usuario}
+                                    subheader= {item.cedula_usuario}
+                                  />
+                                  
+                                  <CardContent>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                      {item.nombre_usuario}
+                                    </Typography>
+                                  </CardContent>
+                                  <CardActions disableSpacing>
+                                    <IconButton aria-label="add to favorites">
+                                      <FavoriteIcon />
+                                    </IconButton>
+                                    <IconButton aria-label="share">
+                                      <ShareIcon />
+                                    </IconButton>
+                                    <IconButton
+                                      className={clsx(classes.expand, {
+                                        [classes.expandOpen]: expanded,
+                                      })}
+                                      onClick={handleExpandClick}
+                                      aria-expanded={expanded}
+                                      aria-label="show more"
+                                    >
+                                      <ExpandMoreIcon />
+                                    </IconButton>
+                                  </CardActions>
+                                  <Collapse in={expanded} timeout="auto" unmountOnExit>
+                                    <CardContent>
+                                      <Typography paragraph>Method:</Typography>
+                                      <Typography paragraph>
+                                        {item.cedula_usuario}
+                                      </Typography>
+                                      <Typography paragraph>
+                                        {item.telefono_usuario}
+                                      </Typography>
+                                      <Typography paragraph>
+                                      {item.mail_usuario}
+                                      </Typography>
+                                      <Typography>
+                                        {item.cedula_usuario}
+                                      </Typography>
+                                    </CardContent>
+                                  </Collapse>
+                                </Card>
+                      );   
+                    })
+                  }
+        </Box>
+       
+      </Box>
+
+   
+    
+  
    
   </Container>
   );
